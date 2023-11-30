d9679803d3be68ea814b8a53479b282e764baa68
Neto Nobre <igornobre2016@hotmail.com>
2023-11-29 23:25:12 -0300

    Back-end feito!

--- back-end/src/models/createDb.js
+++ back-end/src/models/createDb.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
 });
 
 connection.connect((err) => {
