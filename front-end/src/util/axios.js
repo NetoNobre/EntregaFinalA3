b5560c17a7e086493794952fcd9a6268b1852693
Neto Nobre <igornobre2016@hotmail.com>
2023-11-30 01:06:32 -0300

    Front-end em construção

---
+++ front-end/src/util/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const URL_BASE = 'http://localhost:3001';
+
+export const loginAxios = async (data) => {
+    const URL = `${URL_BASE}/user/login`;
+    const response = await axios.post(URL, data);
+    console.log(response +'axios');
+    return response;
+  };
